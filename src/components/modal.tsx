5dc5f7282a334129849b040bf6dde1f47ae982ec
gattaloukik123 <loukik.gatta@gmail.com>
2024-03-31 03:31:01 +0530

    changes

---
+++ src/components/modal.tsx
@@ -0,0 +1,9 @@
+{/* <div className="flex justify-center items-center h-screen">
+      <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={openModal}>
+        Open Modal
+      </button>
+      <Modal isOpen={modalOpen} onClose={closeModal}>
+        <h1 className="text-2xl mb-4">Modal Content</h1>
+        <p>This is the content of the modal.</p>
+      </Modal>
+    </div> */}
