fdd966e6e0725a979cd69ebb6f6987fa84b6153e
gattaloukik123 <loukik.gatta@gmail.com>
2024-03-31 04:55:03 +0530

    made changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 import Image from "next/image";
+import localFont from '@next/font/local'
+
+const nortblack = localFont({
+  src: [
+    {
+      path: 'FFNort-Black.ttf',
+      weight: '400'
+    }
+  ],
+  variable: '--font-nort-black'
+})
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className={`z-10 max-w-5xl w-full items-center justify-end ${nortblack.variable} text-sm lg:flex`}>
         <a 
           href="/" 
-          className="fixed right-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+          className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
           rel="noopener noreferrer"
         >
           Partner With Us
@@ -66,7 +77,7 @@ export default function Home() {
 
         <a
           href=""
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 col-start-1 col-end-2"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -77,9 +88,19 @@ export default function Home() {
           </p>
         </a>
 
+        {/* <div className="group rounded-lg">
+          <Image
+            className="relative"
+            src="/fyt.svg"
+            alt="Communitie Logo"
+            width={450}
+            height={30}
+          />
+        </div> */}
+
         <a
           href=""
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 col-start-3 col-end-4"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
